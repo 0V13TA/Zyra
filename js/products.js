a747e4cb5941e800c0e87f51b5411e4b2816b3be
0V13TA <vincentpaulomorogbe6@gmail.com>
2026-03-10 20:57:45 +0000

    Added a products file to handle all the products

---
+++ js/products.js
@@ -0,0 +1,24 @@
+/**
+ * @typedef {Object} productObj
+ * @property {string} name
+ * @property {number} price
+ * @property {string} image
+ * @property {number} quantity
+ * @property {"NGN" | "USD" | "GBP" | "EUR"} currency
+ * @property {"top" | "dress" | "shirt" | "trouser" | "skirt"} type
+ */
+
+/**
+ * Finds A product by a certain filter value`\`
+ *
+ * @param {productObj[]} product  - The array to want to filter
+ * @param {productObj?} filterValues - The filter Parameters
+ * @returns {productObj[]} An array of products that meet the filter requirement
+ *
+ * @example
+ * ```
+ *
+ *
+ * ```
+ */
+function getProducts(product, filterValues) {}
