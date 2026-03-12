13d819baf75c7ca461ae64f9a41410b06728eef7
0V13TA <vincentpaulomorogbe6@gmail.com>
2026-03-12 16:12:01 +0000

    Made it easier to use the custom modules. All that matters now is to import init.js

---
+++ js/init.js
@@ -0,0 +1,14 @@
+const scriptsLink = [
+  "../js/data.js",
+  "../js/cart.js",
+  "../js/components/currency-selector.js",
+  "../js/components/footer.js",
+  "../js/components/navbar.js",
+  "../js/layout.js",
+];
+
+scriptsLink.forEach((link) => {
+  const script = document.createElement("script");
+  script.src = link;
+  document.head.append(script);
+});
---
+++ js/layout.js
@@ -0,0 +1,4 @@
+const zyraNavbar = document.createElement("zyra-navbar");
+const zyraFooter = document.createElement("zyra-footer");
+document.body.prepend(zyraNavbar);
+document.body.append(zyraFooter);
